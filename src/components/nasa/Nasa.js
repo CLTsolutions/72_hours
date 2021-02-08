9108382e91c8335048db47d3efd785eb93cfe3e9
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-02-07 19:48:22 -0500

    added folder structure

---
+++ src/components/nasa/Nasa.js
@@ -0,0 +1,15 @@
+import './Nasa.css'
+
+// Function name matches file name
+const Nasa = () => {
+    // return must have one parent element
+    return (
+        <div>
+            Also a change
+            72 hour project
+        </div>
+    )
+}
+
+// Makes it available for import
+export default Nasa;
