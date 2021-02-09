eeb2f9b130e557e4f364b50f3bef01819dbe4462
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-02-08 21:53:02 -0500

    fixed location in nasa.js

--- src/App.js
+++ src/App.js
@@ -9,22 +9,27 @@ import Nasa from './components/nasa/Nasa'
 // Defining our <App /> component the function name matches the file name
 function App() {
   // All functional components need to return jsx with one parent element
-  const [locate, setLocate] = useState()
+  const [location, setLocation] = useState({latitude: 0, longitude: 0})
   
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
-      setLocate(position);
+      setLocation(
+        {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      );
     })
-  }, [location])
+  }, [])
 
   return ( 
     <div className="App"> {/* Parent Element. Also we can't use the word class, so we use className in jsx*/}
       {/* Navbar is our imported component*/}
       <Navbar />
-      {locate?.coords.longitude}
-      {locate?.coords.latitude}
-      <Nasa location={locate} />
+      {location.latitude}
+      {location.longitude}
+      <Nasa long={location.longitude} lat={location.latitude} />
     </div>
   );
 }
--- src/components/nasa/Nasa.js
+++ src/components/nasa/Nasa.js
@@ -2,20 +2,33 @@ import { useEffect, useState } from 'react';
 
 import './Nasa.css'
 
-const Nasa = ({location}) => {
-    const url = `https://api.nasa.gov/planetary/earth/assets?lon=${location.longitude}&lat=${location.latitude}&date=2014-02-01&dim=0.19&api_key=QHaRH5NLVJMiqZNxZ9I84QtHlOsxTH4mvhHO4z1Y`;
-    const [imgUrl, setImgUrl] = useState();
+const Nasa = ({lat, long}) => {
+    // const url = `https://api.nasa.gov/planetary/earth/assets?lon=${long}&lat=${lat}&date=2019-03-11&&dim=0.05&api_key=QHaRH5NLVJMiqZNxZ9I84QtHlOsxTH4mvhHO4z1Y`;
+    const url = `https://api.nasa.gov/planetary/earth/assets?lon=${long}&lat=${lat}&date=2014-02-01&dim=0.15&api_key=QHaRH5NLVJMiqZNxZ9I84QtHlOsxTH4mvhHO4z1Y`;
+    const [imgUrl, setImgUrl] = useState('');
+
+    const pleaseWork = async() => {
+        const response = await fetch(url)
+        const picture = await response.json()
+        setImgUrl(picture.url)
+    }
 
     useEffect(() => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                setImgUrl(data.url)
-            })
-            .catch(error => console.log(error));
-    }, []);
+        console.log(lat);
+        // fetch(url)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log(data)
+        //         setImgUrl(data.url)
+        //     })
+        //     .catch(error => console.log(error));
+
+        pleaseWork()        
+
+    }, [lat, long, url]);
     
+    console.log(imgUrl);
+
     return (
         <div>
             <h1>NASA</h1>
